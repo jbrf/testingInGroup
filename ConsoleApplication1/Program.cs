9d9992c01bcebb7f88425b1299a7772f8c196ebd
unknown <marko.bijelic@R9ZBVHG.learnet.se>
2013-09-18 10:22:04 +0200

    added Audi R8 to the car selection

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -22,12 +22,18 @@ class Nissan : car
     public Nissan(string modelname,int numOfDoors): base(modelname,numOfDoors) { }
 }
 
+class Audi : car
+{
+    public Audi(string modelname, int numOfDoors) : base(modelname, numOfDoors) { }
+
+}
 class Program
 {
     static void Main()
     {
         car Nissan = new Nissan("GTR",3);
+        car Audi = new Audi("r8", 2);
         Console.WriteLine(Nissan.Modelname + " "+ Nissan.NumberOfDoors);
-
+        Console.WriteLine(Audi.Modelname + " " + Audi.NumberOfDoors);
     }
 }
