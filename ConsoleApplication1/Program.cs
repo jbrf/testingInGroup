0a6851416287ec853b696768d8322cb45d2b01a9
Johan Björklund <johan@jsbmaleri.se>
2013-09-18 10:16:09 +0200

    Added some doors to the car Nissan GTR, also fixed constructor for car and Nissan

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -3,16 +3,31 @@
 
 class car
 {
-    car Nissan = new car();
+   //public car Nissan {get;set;}
+
+    public string Modelname { get; set; }
+    public int NumberOfDoors { get; set; }
 
 
+   public car(string x,int numOfDoors)
+   {
+       Modelname = x;
+       NumberOfDoors = numOfDoors;
+   }
+
+}
+
+class Nissan : car 
+{
+    public Nissan(string modelname,int numOfDoors): base(modelname,numOfDoors) { }
 }
 
 class Program
 {
     static void Main()
     {
-
+        car Nissan = new Nissan("GTR",3);
+        Console.WriteLine(Nissan.Modelname + " "+ Nissan.NumberOfDoors);
 
     }
 }
