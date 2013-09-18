48451ecd65291fec72a4d66a7bd3b40aff94e88a
unknown <marko.bijelic@R9ZBVHG.learnet.se>
2013-09-18 11:26:35 +0200

    yrvggrcgr

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -6,12 +6,14 @@ class car
  
     public string Modelname { get; set; }
     public int NumberOfDoors { get; set; }
- 
+    public int NumberOfHorsePower { get; set; }
+
 
    public car(string x,int numOfDoors)
    {
        Modelname = x;
        NumberOfDoors = numOfDoors;
+       NumberOfHorsePower = NumberOfHorsePower;
    }
 
 }
@@ -23,7 +25,7 @@ class Fiat : car
 
 class Nissan : car 
 {
-    public Nissan(string modelname,int numOfDoors): base(modelname,numOfDoors) { }
+    public Nissan(string modelname,int numOfDoors, int numOfHP): base(modelname,numOfDoors) { }
 }
 
 class Audi : car
@@ -42,13 +44,14 @@ class Program
 {
     static void Main()
     {
-        car Nissan = new Nissan("Nissan GTR",2);
+        car Nissan = new Nissan("Nissan GTR", 2, 530);
         car Audi = new Audi("Audi r8", 2);
         car Fiat = new Fiat("Fiat PUNTO 1,2", 5);
         car Ferrari = new Ferrari ("Ferrari 458 Italia", 2);
-        Console.WriteLine(Nissan.Modelname + " has "+ Nissan.NumberOfDoors + " doors" );
+        Console.WriteLine(Nissan.Modelname + " has "+ Nissan.NumberOfDoors + " doors" + " The 2013 version has 530 HorsePower");
         Console.WriteLine(Audi.Modelname + " has " + Audi.NumberOfDoors + " doors");
         Console.WriteLine(Fiat.Modelname + " has "+ Fiat.NumberOfDoors + " doors" );
         Console.WriteLine(Ferrari.Modelname + " has " + Ferrari.NumberOfDoors + " door ");
+        
     }
 }
